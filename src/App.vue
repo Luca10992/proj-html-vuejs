402f9a4819383bfdf90889a854eef3ea6e0a7646
Luca10992 <lucalillo.99.1@gmail.com>
2024-02-19 11:40:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppHeader.vue";
+import AppFooter from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+
+<template>
+  <header>
+    <AppHeader></AppHeader>
+  </header>
+
+  <main>
+    <AppMain></AppMain>
+  </main>
+
+  <footer>
+    <AppFooter></AppFooter>
+  </footer>
+</template>
+
+<style lang="scss">
+@use "../src/styles/generals.scss";
+</style>
